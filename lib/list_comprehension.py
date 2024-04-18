e8cef38539285c44e8ddc16948511dfea9d5a216
akn416 <52968393+akn416@users.noreply.github.com>
2024-04-18 16:35:17 -0400

    Update list_comprehension.py

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    new_list = list()
+    new_list = [num_list[n] for n in range(len(num_list)) if num_list[n] % 2 == 0]
+    return new_list
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = list()
+    new_list = [sentence_list[n] + '!' for n in range(len(sentence_list))]
+    return new_list
